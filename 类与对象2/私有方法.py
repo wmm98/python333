57a2a087e8da77bc95252a2c41050c0a53cbeb85
lenovo <792545884@qq.com>
2019-03-18 15:57:47 +0800

    0000

---
+++ 类与对象2/私有方法.py
@@ -0,0 +1,13 @@
+class Person:
+    __age = 34
+
+    def __run(self):
+        print("跑步的鱼-------------")
+
+p = Person()
+# p.__run()   # AttributeError: 'Person' object has no attribute '__run'
+
+print(Person.__dict__)
+#  {'__module__': '__main__', '_Person__age': 34, '_Person__run': <function Person.__run at 0x000001EA242552F0>
+p._Person__run()
+# 跑步的鱼-------------
